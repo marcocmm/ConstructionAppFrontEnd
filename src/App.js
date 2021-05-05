cffeda49b7eeac586989b786c4115f7950329271
marcocmmattos <75364222+marcocmmattos@users.noreply.github.com>
2021-05-05 22:10:39 +0100

    Auth context, routes, basic structure

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Fragment } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Login from "./components/pages/Login";
 import NotFound from "./components/pages/NotFound";
+import AuthState from "./context/auth/AuthState";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Fragment>
-        <div className="container">
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route component={NotFound} />
-          </Switch>
-        </div>
-      </Fragment>
-    </Router>
+    <AuthState>
+      <Router>
+        <Fragment>
+          <div className="container">
+            <Switch>
+              <PrivateRoute exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route component={NotFound} />
+            </Switch>
+          </div>
+        </Fragment>
+      </Router>
+    </AuthState>
   );
 }
 
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Home = () => {
-  return <div className="grid-2">Home</div>;
+  return <div className="grid-2">Home!</div>;
 };
 
 export default Home;
